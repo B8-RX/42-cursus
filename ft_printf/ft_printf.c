0cb972753551a5eaf952132349228fe779ab4d50
YanZolo <sofiane.sghiouar@gmail.com>
2023-12-09 21:23:38 +0100

    ft_printf: fix incrementation of index in ft_printf function from i+= count to i+= 2 when encountering '%', add ft_print_nbr function

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -21,7 +21,7 @@ int	ft_print_char(char c)
 }
 
 int	ft_print_str(char *str)
-{:
+{
 	int	count;
 
 	count = 0;
@@ -30,8 +30,39 @@ int	ft_print_str(char *str)
 		count++;
 		ft_print_char(*str);
 		str++;
-	}
+	} 
 	return (count);
+}
+
+int	ft_print_nbr(int nb, int size)
+{
+
+	char	num;
+	
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return (11);
+	}
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		size++;
+		ft_print_nbr(-nb, size);	
+	}
+	if (nb > 9)
+	{
+		num = nb % 10 + '0';
+		write(1, &num, 1);
+		size++;
+		ft_print_nbr(nb / 10, size);
+	}
+	else
+	{
+		num = nb + '0';
+		write(1, &num, 1);
+	}
+	return (size);
 }
 
 int	ft_handle_format(char f, va_list args)
@@ -39,21 +70,21 @@ int	ft_handle_format(char f, va_list args)
 	if (f == 'c')
 		return (ft_print_char(va_arg(args, int))); 
 	if (f == 's')
-		return (ft_print_str(va_arg(args, (char *)));
+		return (ft_print_str((char *)va_arg(args, void *)));
 	if (f == 'd')
-		return (ft_print_deci(va_arg(args, float)));
-	if (f == 'p')
-		return (ft_print_nbr_base(va_arg(args, (void *), 16));
-	if (f == 'i')
-		return (ft_print_nbr(va_arg(args, int), 10)); 
-	if (f == 'u')
-		return (ft_print_deci(va_arg(args, float)));
-	if (f == 'x')
-		return (ft_print_nbr_base(va_arg(args, int), 16));
-	if (f == 'X')
-		return (ft_print_nbr(va_arg(args,int),16));
-	if (f == '%')
-		return (write(1, &(*format), 1));
+		return (ft_print_nbr(va_arg(args, int), 0));
+	// if (f == 'p')
+	// 	return (ft_print_nbr_base(va_arg(args, (void *), 16));
+	// if (f == 'i')
+	// 	return (ft_print_nbr(va_arg(args, int), 10)); 
+	// if (f == 'u')
+	// 	return (ft_print_nbr(va_arg(args, float)));
+	// if (f == 'x')
+	// 	return (ft_print_nbr_base(va_arg(args, int), 16));
+	// if (f == 'X')
+	// 	return (ft_print_nbr(va_arg(args,int),16));
+	// if (f == '%')
+	// 	return (write(1, &(*format), 1));
 	return (0);
 }
 
@@ -61,33 +92,41 @@ int	ft_printf(const char *format, ...)
 {
 	va_list	args;
 	int		count;
+	int		i;
 
+	i = 0;
 	count = 0;
 	va_start(args, format);
-	while (*format != '\0')
+	while (format[i] != '\0')
 	{
-		if(*format == '%')
+		if(format[i] == '%')
 		{	
-			format++;
-			count+= ft_handle_format(*format, args);
+			count+= ft_handle_format(format[i + 1], args);
+			i += 2;
 		}
 		else
 		{
-			count+= write(1, &(*format), 1);
-			format++;
+			count+= write(1, &format[i], 1);
+			i++;
 		}
 	}
+	va_end(args);
+	printf("printf: count => %d\n", count);
 	return (count);
 }
 
 int	main(void)
 {
-//	int		arg1;
-	char	arg2[] = "test";
-	int		count;
+	int	count;
+	// char	c = 'z';	
+	// char	str[] = "test";
+	int		decimal = -2147483648;
 
+	count = 0;
 //	arg1 = 5;
-	count = ft_printf("%c", 'z');
-	ft_printf("result :%d\n", count);
+	// count = ft_printf("%c\n", c);// test char
+	// count = ft_printf("%s\n", arg2); // test string
+	count = ft_printf("%d\n", decimal); // test decimal
+	printf("result :%d\n", count);
 	return (0);
 }
