4a570f27940214bcc39a2434eebb8e38f2c46bde
YanZolo <sofiane.sghiouar@gmail.com>
2023-12-10 21:37:14 +0100

    ft_printf: add external tests for ft_printf

---
+++ tests/test_ft_printf/main.c
@@ -0,0 +1,135 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssghioua <ssghioua@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/10 20:09:12 by ssghioua          #+#    #+#             */
+/*   Updated: 2023/12/10 20:09:17 by ssghioua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_printf/libft.h"
+
+int	main(void)
+{
+	int	count_ft_printf = 0;
+	int	count_printf = 0;
+	
+//------------------------------test char--------------------------------//
+
+	// char	c = 'z';	
+	// count_ft_printf = ft_printf("%c\n", c);
+	// printf("result ft_printf:%d\n", count_ft_printf); 
+
+	//---- test with original printf function------//
+	
+	// count_printf = printf("%c\n", c);
+	// printf("result printf :%d\n", count_printf);
+
+
+//------------------------------test string--------------------------------//
+	
+	// char	str[] = "test 1 2 3 abracadabra";
+	// count_ft_printf = ft_printf("%s\n", str); // test string
+	// printf("result ft_printf:%d\n", count_ft_printf); 
+	
+	//---- test with original printf function------//
+	
+	// count_printf = printf("%s\n", str);
+	// printf("result printf :%d\n", count_printf);
+
+
+
+//------------------------------test INT_MIN--------------------------------//
+	
+	int		int_min = -2147483648;
+	count_ft_printf = ft_printf("%d\n", int_min); // test INT_MIN
+	printf("result ft_printf:%d\n", count_ft_printf); 
+	
+	//---- test with original printf function------//
+	
+	count_printf = printf("%d\n", int_min);
+	printf("result printf :%d\n", count_printf);
+
+//------------------------------test INT_MAX--------------------------------//
+	
+	// int		int_max = 2147483647;
+	// count_ft_printf = ft_printf("%d\n", int_max); // test INT_MAX
+	// printf("result ft_printf:%d\n", count_ft_printf); 
+	
+	//---- test with original printf function------//
+	
+	// count_printf = printf("%d\n", int_max);
+	// printf("result printf :%d\n", count_printf);
+
+//------------------------------test positive num--------------------------------//
+	
+	// int		nbr_positive = 42;
+	// count_ft_printf = ft_printf("%d\n", nbr_positive); // test positive num
+	// printf("result ft_printf:%d\n", count_ft_printf); 
+	
+	//---- test with original printf function------//
+	
+	// count_printf = printf("%d\n", nbr_positive);
+	// printf("result printf :%d\n", count_printf);
+
+//------------------------------test negative num--------------------------------//
+	
+	// int		nbr_negative = -421154562;
+	// count_ft_printf = ft_printf("%d\n", nbr_negative); // test negative num
+	// printf("result ft_printf:%d\n", count_ft_printf); 
+	
+	//---- test with original printf function------//
+	
+	// count_printf = printf("%d\n", nbr_negative);
+	// printf("result printf :%d\n", count_printf);
+
+//------------------------------test unsigned int--------------------------------//
+	
+	// int		nbr_unsigned = -42;
+	// count_ft_printf = ft_printf("%u\n", nbr_unsigned); // test unsigned int
+	// printf("result ft_printf:%d\n", count_ft_printf); 
+	
+	//---- test with original printf function------//
+	
+	// count_printf = printf("%u\n", nbr_unsigned);
+	// printf("result printf :%d\n", count_printf);
+
+//------------------------------test hexa lower--------------------------------//
+	
+	// int		nbr_positive = 42;
+	// count_ft_printf = ft_printf("%x\n", nbr_positive); // test hexa lower case
+	// printf("result ft_printf:%d\n", count_ft_printf); 
+	
+	//---- test with original printf function------//
+	
+	// count_printf = printf("%x\n", nbr_positive);
+	// printf("result printf :%d\n", count_printf);
+
+//------------------------------test hexa upper--------------------------------//
+	
+	// int		nbr_positive = 42;
+	// count_ft_printf = ft_printf("%X\n", nbr_positive); // test hexa upper case
+	// printf("result ft_printf:%d\n", count_ft_printf); 
+	
+	//---- test with original printf function------//
+	
+	// count_printf = printf("%X\n", nbr_positive);
+	// printf("result printf :%d\n", count_printf);
+
+//------------------------------test pointer--------------------------------//
+
+	// int		nbr_positive = 42;
+	// int		*ptr = &nbr_positive;
+	// count_ft_printf = ft_printf("%p\n", ptr); // test pointer	
+	// printf("result ft_printf:%d\n", count_ft_printf); 
+	
+	//---- test with original printf function------//
+	
+	// count_printf = printf("%p\n", ptr);
+	// printf("result printf :%d\n", count_printf);
+	
+	return (0);
+}
