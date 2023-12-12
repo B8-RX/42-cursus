02d18619208b9c3c0ca19ee2363bd6ddc7c2f1ef
YanZolo <sofiane.sghiouar@gmail.com>
2023-12-12 19:13:55 +0100

    ft_printf: fix ft_print_hexa when input is 16

--- ft_printf/ft_print_hexa.c
+++ ft_printf/ft_print_hexa.c
@@ -19,7 +19,7 @@ int	ft_print_hexa(unsigned long nb, int size, char format, char *base)
 	base_len = ft_strlen(base);
 	if (format == 'p' && !nb)
 		return (write(1, "(nil)", 5));
-	if (nb > base_len)
+	if (nb >= base_len)
 	{
 		size = 1 + ft_print_hexa(nb / base_len, size, format, base);
 		write(1, &base[nb % base_len], 1);	
