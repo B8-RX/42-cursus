fb6dd8c1d927a8b0877dc56e4f324b7b1ef583ca
YanZolo <sofiane.sghiouar@gmail.com>
2023-12-16 02:42:25 +0100

    get_next_line: first commit

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssghioua <ssghioua@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/16 02:31:51 by ssghioua          #+#    #+#             */
+/*   Updated: 2023/12/16 02:31:53 by ssghioua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 13
+#endif // !BUFFER_SIZE
+
+char	*get_next_line(int fd);		
+
+
+
+#endif // !GET_NEXT_LINE_H
