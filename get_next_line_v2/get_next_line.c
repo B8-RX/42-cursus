9688ac5bbc779a30af56bc463b8ef930d471d683
YanZolo <sofiane.sghiouar@gmail.com>
2023-12-28 12:04:18 +0100

    gnl_v2: improve semantic, some tests failed in francinette and tripouille

--- get_next_line_v2/get_next_line.c
+++ get_next_line_v2/get_next_line.c
@@ -38,54 +38,54 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
-char	*ft_update_memo_buff(char *memo_buff, char *buff, size_t last_index)
+char	*ft_update_static_buff(char *static_buff, char *buff, size_t last_index)
 {
 	char	*new;
 
-	new = ft_strjoin((memo_buff + last_index), buff);
-	free(memo_buff);
+	new = ft_strjoin((static_buff + last_index), buff);
+	free(static_buff);
 	return (new);
 }
 
-char	*ft_update_buff(char *memo_buff, char *buff, int lf)
+char	*ft_update_buff(char *static_buff, char *buff, int found_lf)
 {
 	char	*new;
 	size_t	size_line;
 
-	if (lf)
-		size_line = ft_strlen(memo_buff)
-			- ft_strlen(ft_strchr(memo_buff, '\n')) + 1;
+	if (found_lf)
+		size_line = ft_strlen(static_buff)
+			- ft_strlen(ft_strchr(static_buff, '\n')) + 1;
 	else
-		size_line = ft_strlen(memo_buff) + ft_strlen(buff);
+		size_line = ft_strlen(static_buff) + ft_strlen(buff);
 	free(buff);
-	new = ft_substr(memo_buff, 0, size_line);
+	new = ft_substr(static_buff, 0, size_line);
 	return (new);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*memo_buff;
+	static char	*static_buff;
 	char		*buff;
 
-	if (fd < 0)
-		return (NULL);
-	if (!memo_buff)
-		memo_buff = ft_strjoin("", "");
+	if (!static_buff)
+		static_buff = ft_strjoin("", "");
 	buff = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
 	if (!buff)
-		return (free(memo_buff), NULL);
-	if ((!read(fd, buff, BUFFER_SIZE) || !*buff) && (*memo_buff == '\0'))
-		return (free(buff), free(memo_buff), NULL);
-	memo_buff = ft_update_memo_buff(memo_buff, buff, 0);
+		return (NULL);
+	if (fd < 0)
+		return (free(buff), NULL);
+	if ((!read(fd, buff, BUFFER_SIZE) || !*buff) && *static_buff == '\0')
+		return (free(buff), NULL);
+	static_buff = ft_update_static_buff(static_buff, buff, 0);
 	if (ft_strchr(buff, '\n'))
 	{
-		buff = ft_update_buff(memo_buff, buff, 1);
-		memo_buff = ft_update_memo_buff(memo_buff, "", ft_strlen(buff));
+		buff = ft_update_buff(static_buff, buff, 1);
+		static_buff = ft_update_static_buff(static_buff, "", ft_strlen(buff));
 	}
 	else
 	{
-		buff = ft_update_buff(memo_buff, buff, 0);
-		memo_buff = ft_update_memo_buff(memo_buff, "", ft_strlen(buff));
+		buff = ft_update_buff(static_buff, buff, 0);
+		static_buff = ft_update_static_buff(static_buff, "", ft_strlen(buff));
 	}
 	return (buff);
 }
