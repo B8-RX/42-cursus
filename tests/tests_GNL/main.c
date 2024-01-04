a830982972f55d10bee9e01581f4867eca5363d1
YanZolo <sofiane.sghiouar@gmail.com>
2024-01-04 23:11:28 +0100

    test_GNL: add possibility to modify BUFFER_SIZE after the make command (i.e. make BUFFER_SIZE=37)

--- tests/tests_GNL/main.c
+++ tests/tests_GNL/main.c
@@ -10,23 +10,21 @@ int	main(void)
 	int		nb_line;
 
 	nb_line = 0;
-	fd = open("./fable_small.txt", O_RDONLY);	if (fd == -1)
+	fd = open("./fable_small.txt", O_RDONLY);
+	if (fd == -1)
 		return (0);
 	next_line = get_next_line(fd);
 	while(next_line != NULL)
 	{
 		nb_line++;
-		
 		printf("\n==========================================================\n\n");
 		printf("total lines |%d|:\n|%s|\n", nb_line, next_line);
 		printf("\n==========================================================\n\n");	
-		
 		free(next_line);
 		next_line = NULL;
 		next_line = get_next_line(fd);
 		if (next_line)
 			printf("/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\\n");
-		
 	}
 	free(next_line);
 	close(fd);
