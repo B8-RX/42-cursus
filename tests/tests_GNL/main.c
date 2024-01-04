3debac62f6726f64c33d96df75f91b03d5269f79
YanZolo <sofiane.sghiouar@gmail.com>
2024-01-04 22:21:32 +0100

    test_GNL: in main.c add while loop to get all the lines from the test file, add new files

--- tests/tests_GNL/main.c
+++ tests/tests_GNL/main.c
@@ -1,21 +1,34 @@
 
 
 
-#include <../get_next_line/get_next_line.h>
+#include "../../get_next_line/gnl_v2_single_static/get_next_line.h"
 
 int	main(void)
 {
 	int		fd;
 	char	*next_line;
-	int		count;
+	int		nb_line;
 
-	count = 0;
-	fd = open("./fable.txt", O_RDONLY);
+	nb_line = 0;
+	fd = open("./fable_small.txt", O_RDONLY);	if (fd == -1)
+		return (0);
 	next_line = get_next_line(fd);
-	count++;
-	printf("line [%d]:\n%s\n", count, next_line);
-	next_line = NULL;
-
+	while(next_line != NULL)
+	{
+		nb_line++;
+		
+		printf("\n==========================================================\n\n");
+		printf("total lines |%d|:\n|%s|\n", nb_line, next_line);
+		printf("\n==========================================================\n\n");	
+		
+		free(next_line);
+		next_line = NULL;
+		next_line = get_next_line(fd);
+		if (next_line)
+			printf("/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\\n");
+		
+	}
+	free(next_line);
 	close(fd);
 	return (0);
 }
