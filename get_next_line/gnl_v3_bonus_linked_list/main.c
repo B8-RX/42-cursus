264bef2aaec4f8dad94ea409a517bfbffb2e946d
YanZolo <sofiane.sghiouar@gmail.com>
2024-01-10 06:08:06 +0100

    gnl_bonus: work on the ft_read_file branch. When initializing stash fd, assign an empty string to buffer fd. Todo: in ft_read_file function check that the linked lists have not lost a link when a list is freed.

--- get_next_line/gnl_v3_bonus_linked_list/main.c
+++ get_next_line/gnl_v3_bonus_linked_list/main.c
@@ -3,12 +3,18 @@
 
 int	main(void)
 {
+<<<<<<< HEAD
 	// int		fd1;
 	// int		fd2;
+=======
+	int		fd1;
+	int		fd2;
+>>>>>>> b7d415a (gnl_bonus: work on the ft_read_file branch. When initializing stash fd, assign an empty string to buffer fd. Todo: in ft_read_file function check that the linked lists have not lost a link when a list is freed.)
 	int		fd3;
 	char	*line;
 	int		i;
 
+<<<<<<< HEAD
 	// fd1 = open("../../tests_GNL/my_tests_gnl/fable_small.txt", O_RDONLY);
 	// fd2 = open("../../tests_GNL/my_tests_gnl/file_test_nl.txt", O_RDONLY);
 	fd3 = open("../../tests_GNL/my_tests_gnl/file_test_no_nl.txt", O_RDONLY);
@@ -27,24 +33,51 @@ int	main(void)
 	// line = get_next_line(fd1);
 	// printf("result: %d: |%s|\n", ++i, line);
 =======
+=======
+	i = 0;
+	fd1 = open("../../tests_GNL/my_tests_gnl/fable_small.txt", O_RDONLY);
+	fd2 = open("../../tests_GNL/my_tests_gnl/file_test_nl.txt", O_RDONLY);
+	fd3 = open("../../tests_GNL/my_tests_gnl/file_test_no_nl.txt", O_RDONLY);
+
+>>>>>>> b7d415a (gnl_bonus: work on the ft_read_file branch. When initializing stash fd, assign an empty string to buffer fd. Todo: in ft_read_file function check that the linked lists have not lost a link when a list is freed.)
 	 
 	line = get_next_line(fd1);
-	printf("result 1: %s\n\n", line);
+	printf("result %d: %s\n\n", ++i, line); // 1
+	// free(line);
+	
 	line = get_next_line(fd2);
-	printf("result 2: %s\n\n", line);
+	printf("result %d: %s\n\n", ++i, line); // 2
+	// free(line);
+	
 	line = get_next_line(fd3);
-	printf("result 3: %s\n\n", line);
-	line = get_next_line(fd3);
-	printf("result 3: %s\n\n", line);
-	line = get_next_line(fd2);
-	printf("result 2: %s\n\n", line);
-	line = get_next_line(fd3);
-	printf("result 3: %s\n\n", line);
+	printf("result %d: %s\n\n", ++i, line); // 3
+	// free(line);
+	
 	line = get_next_line(fd1);
+<<<<<<< HEAD
 	printf("result 1: %s\n\n", line);
 	line = get_next_line(fd2);
 	printf("result 2: %s\n\n", line);
 >>>>>>> d3ac247 (gnl_bonus: update the linked list in ft_check_fd_stash function, free memory, freeing linked_list if ft_check_fd_stash function return NULL)
+=======
+	printf("result %d: %s\n\n", ++i, line); // 4
+	// free(line);
+
+	line = get_next_line(fd1);
+	printf("result %d: %s\n\n", ++i, line); // 5
+	// free(line);
+	// 
+	// line = get_next_line(fd3);
+	// printf("result %d: %s\n\n", ++i, line); // 6
+	// // free(line);
+	// 
+	// line = get_next_line(fd1);
+	// printf("result %d: %s\n\n", ++i, line); // 7
+	// // free(line);
+	// 
+	// line = get_next_line(fd2);
+	// printf("result %d: %s\n\n", ++i, line); // 8
+>>>>>>> b7d415a (gnl_bonus: work on the ft_read_file branch. When initializing stash fd, assign an empty string to buffer fd. Todo: in ft_read_file function check that the linked lists have not lost a link when a list is freed.)
 	// free(line);
 	// line = get_next_line(fd1);
 	// printf("result %d: |%s|\n", ++i, line);
