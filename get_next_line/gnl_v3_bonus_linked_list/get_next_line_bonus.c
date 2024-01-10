480f5a2bd3ca63193d14f41db03c5608b2912eb2
YanZolo <sofiane.sghiouar@gmail.com>
2024-01-10 03:04:42 +0100

    gnl_bonus: function ft_check_fd_stash implemeted, tests ok

--- get_next_line/gnl_v3_bonus_linked_list/get_next_line_bonus.c
+++ get_next_line/gnl_v3_bonus_linked_list/get_next_line_bonus.c
@@ -18,6 +18,19 @@ char	*get_next_line(int fd)
 	Stash_list			*Current_fd;
 	char				*line;
 
+	if (fd == -2 && Stash)
+	{
+		Stash_list	*Temp;
+
+			while (Stash && Stash != NULL)
+			{
+				Temp = Stash;
+				free(Temp -> Fd_stash);
+				free(Temp);
+				Stash = Stash -> next;
+			}
+			return (NULL);
+	}
 	if (fd < 0 || !BUFFER_SIZE || BUFFER_SIZE <= 0)
 		return (NULL);
 	if (!Stash && !ft_init_stash(&Stash, fd))
