9f671e54336c7902f7ece625e8cf548e00917323
YanZolo <sofiane.sghiouar@gmail.com>
2024-04-12 05:19:53 +0200

    push_swap[ci]: implementing circular linked list.

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -17,7 +17,7 @@
 typedef struct s_stack {
 	int				value;
 	int				index;
-	// struct s_stack	*previous;
+	struct s_stack	*previous;
 	struct s_stack	*next;
 }	t_stack;
 
@@ -25,10 +25,10 @@ typedef struct s_stack {
 t_stack	*ft_init_stack(t_stack **stack);
 t_stack	*ft_handle_args(int args_len, char **args);
 t_stack	*ft_get_last_lst(t_stack *stack);
-t_stack	**ft_add_lst_back(t_stack **stack, int value);
-t_stack	**ft_add_lst_front(t_stack **stack, int value);
+t_stack	**ft_add_lst_back(t_stack **stack, int value, int index);
+t_stack	**ft_add_lst_front(t_stack **stack, int value, int index);
 int		ft_count_words(char *str, char splitter);
-int		ft_is_digit(char c);
+int		ft_is_digit(char *str);
 char	*ft_print_error(void);
 void	ft_print_values_lst(t_stack *stack);
 int		ft_count_spaces(char *str);
@@ -37,6 +37,8 @@ char	**ft_split(char *str, char splitter);
 void	*ft_free_array_str(char **array);
 void	ft_free_stack(t_stack *stack);
 int		ft_append_child(char *child, char *value, char splitter);
+int		ft_atoi(const char *str);
+size_t	ft_strlen(const char *str);
 
 
 
